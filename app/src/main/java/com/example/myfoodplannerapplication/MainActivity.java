86b5481ce40ed88e891576cd3ed0e8321bd99497
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-02-14 09:13:19 +0200

    custom the bottom navigation bar

--- app/src/main/java/com/example/myfoodplannerapplication/MainActivity.java
+++ app/src/main/java/com/example/myfoodplannerapplication/MainActivity.java
@@ -8,16 +8,21 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     BottomNavigationView bottomNavigationView;
+    private FloatingActionButton fabAdd;
+    CoordinatorLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,27 +33,30 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+        layout = findViewById(R.id.cor_layout);
+        fabAdd = findViewById(R.id.addFragment);
 
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+        fabAdd.setOnClickListener(view -> {
+            navController.navigate(R.id.addFragment);
+        });
 
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
-            if (destination.getId() == R.id.homeFragment) {
+            if (destination.getId() == R.id.homeFragment | destination.getId() == R.id.searchFragment | destination.getId() == R.id.calenderFragment | destination.getId() == R.id.favoriteFragment) {
                 toolbar.setVisibility(View.VISIBLE);
+                layout.setVisibility(View.VISIBLE);
             } else {
                 toolbar.setVisibility(View.GONE);
+                layout.setVisibility(View.GONE);
             }
         });
 
-        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
-            if (destination.getId() == R.id.homeFragment || destination.getId() == R.id.addFragment || destination.getId() == R.id.favoriteFragment || destination.getId() == R.id.calenderFragment || destination.getId() == R.id.profileFragment) {
-                bottomNavigationView.setVisibility(View.VISIBLE);
-            } else {
-                bottomNavigationView.setVisibility(View.GONE);
-            }
-        });
     }
 
     @Override
